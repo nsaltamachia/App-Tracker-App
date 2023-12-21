4c044cb8fb0d50ab0b9d889af56d6a86caabdc6d
NickSaltamachia <nsaltamachia@gmail.com>
2023-12-21 09:08:28 -0600

    more code cleaning

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -25,19 +25,6 @@ async function handleSubmit(evt) {
   }
 }
 
-  // async function handleSubmit(evt) {
-  //   evt.preventDefault();
-  //   try {
-  //     // The promise returned by the signUp service method
-  //     // will resolve to the user object included in the
-  //     // payload of the JSON Web Token (JWT)
-  //     const user = await usersService.login(credentials);
-  //     setUser(user);
-  //   } catch {
-  //     setError("Log In Failed - Try Again");
-  //   }
-  // }
-
   return (
     <div>
       <div className="form-container">
